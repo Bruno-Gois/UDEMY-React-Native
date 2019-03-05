9d875f0fce84c05260639846d45d60664b9eca9a
bruno.gois <brunovgois@gmail.com>
2019-03-05 00:41:32 -0300

    Authentication App

---
+++ auth/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Auth app!</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
